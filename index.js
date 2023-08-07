178e42c9f2710544ab640e39131e72101a716993
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-07 20:37:12 +0530

    css and home UI added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express(); //set the firing of the express
 app.set("view engine", "ejs"); //told the app to use ejs as the view engine
 app.set("views", path.join(__dirname, "views")); //specify the path for the views folder
 
+//set up the middleware to read a request from the form. it read the form data then parses into the key value pair.
+app.use(express.urlencoded()); //app.use signify the middleware, express.urlencoded takes the request and read or analyze the data.
+app.use(express.static("assets")); //included the static file using middleware //it tells the app to go back and look to the asserts folder whenever you need the static files
+
 var contactList = [
   {
     name: "cheshta",
@@ -25,6 +29,15 @@ app.get("/", function (req, res) {
     title: "Contacts List",
     contact_list: contactList,
   });
+});
+
+app.post("/create-contact", function (req, res) {
+  contactList.push({
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+  console.log(contactList);
+  return res.redirect("back");
 });
 
 app.listen(port, function (err) {
